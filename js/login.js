24134227b6a1d825974cea706f7dc74c98b1163c
HERNANDIITO <pablohhgg@gmail.com>
2024-04-12 11:32:55 +0200

    login y corrección de búsqueda
    login:
    - A: Done
    - B: Done
    busca:
    corrección de busqueda por usuario

--- js/login.js
+++ js/login.js
@@ -1,12 +1,23 @@
-function login( evt ) {
+function login(event) {
+    event.preventDefault();
+    
+    const form = new FormData(document.querySelector(".form"));
+    const url = "api/usuarios/login";
+    const xhr = new XMLHttpRequest();
+
     xhr.open('POST', url, true);
     xhr.responsetype = 'json';
     xhr.onload = function() {
-        let r = xhr.respone;
-        console.log(r);
+        let r = xhr.response;
+        r = JSON.parse(r);
         if ( r.RESULTADO == 'OK' ) {
             sessionStorage['datosUsu'] = JSON.stringify(r);
+            alert(`Último inicio de sesión: ${r.ULTIMO_ACCESO}`)
+            mustLogOut();
+        } else {
+            alert(`Error: ${r.DESCRIPCION}`)
         }
     }
-    xhr.send(fd); // formData
+    
+    xhr.send(form); // formData
 }
